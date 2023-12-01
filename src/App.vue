421c98b36be895488a6a241789e5be2fd141deb7
Kehinde Balogun <balogunkehinde3@gmail.com>
2023-12-01 16:05:55 +0100

    created the Header component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,37 @@
 
 <template>
-   <h1>Hello World</h1>
+   <Header :date="date" />
 </template>
 
 <script>
+  import Header from './components/Header.vue'
 
+   export default {
+    components: {Header},
+    data(){
+      return {
+        date: '',
+      }
+    },
+    mounted() {
+      const options = {weekday: "long", month:"short", day:"numeric"};
+      const today = new Date();
+      this.date = today.toLocaleDateString("en-Us", options);
+    }
+   }
 </script>
 
-<style scoped>
+<style>
+
+   body{
+    background-color: rgba(0,0,0,0.1);
+    font-family: 'Titillium Web', sans-serif;
+  }
+
+  #app{
+    padding:10px;
+    width:380px;
+    margin:0 auto;
+  }
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="header">
+        
+        <div class="date">{{ date }}</div>
+    </div>      
+</template>
+
+<script>
+export default {
+    props: ["date"]
+
+}
+</script>
+
+<style>
+  .header{
+    width: 380px;
+    height: 200px;
+    background-image: url('../assets/bg1.jpg');
+    background-size: 100% 200%;
+    background-repeat: no-repeat;
+    border-radius: 15px 15px 0 0;
+    position: relative;
+    }
+
+    .date{
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    color: #FFF;
+    font-size: 25px;
+    font-family: 'Titillium Web', sans-serif;
+    }
+</style>
